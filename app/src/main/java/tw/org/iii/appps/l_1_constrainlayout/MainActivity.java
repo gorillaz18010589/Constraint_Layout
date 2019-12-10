8a7b9df5f3bbe00fee34054337054e39e86b4e8c
Jasonkidd <gorillaz1801058@gmail.com>
2019-12-10 23:29:50 +0800

    11/29

---
+++ app/src/main/java/tw/org/iii/appps/l_1_constrainlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package tw.org.iii.appps.l_1_constrainlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
